fd62c6d0421cd36c771a5d2cc594ee1dd541fb87
Tarun Ravi <tarunvravi@gmail.com>
2018-08-14 11:04:30 -0400

    Dialogflow Fulfillment Code
    This allows Dialogflow and Firebase to communicate

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+//Database Connection
+const admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'ws://DATABASE URL HERE'
+});
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+ function handleLocation(agent){
+     const sentLocation = agent.parameters.location;
+     agent.add('Thank You...');
+
+      return admin.database().ref('BROADNAME').transaction((location) => {
+      if(location !== null) {
+        let newLocation = sentLocation;
+        location.country = newLocation;
+        
+        agent.add(`Viewing ` + newLocation + ' on Liquid Galaxy');
+      }
+      return location;
+    }, function(error, isSuccess) {
+      console.log('Added Location to Database: ' + isSuccess);
+    });
+ }
+
+  let intentMap = new Map();
+  intentMap.set('NAMEOFINTENT', handleLocation);
+  
+ 
+  agent.handleRequest(intentMap);
+});
